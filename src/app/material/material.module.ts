341c7a36dbab550adfd2e33b0fb261dfe4d069b3
emkeris <emkeris@gmail.com>
2019-05-05 19:54:23 +0100

    moments added

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
-import { MatSidenavModule, MatCheckboxModule, MatButtonModule, MatToolbarModule, MatFormFieldModule} from '@angular/material';
+import {  MatSidenavModule,
+          MatCheckboxModule,
+          MatButtonModule,
+          MatToolbarModule,
+          MatFormFieldModule,
+          MatInputModule} from '@angular/material';
 
 @NgModule({
-  imports: [ MatSidenavModule, MatCheckboxModule, MatButtonModule, MatToolbarModule, MatFormFieldModule],
-  exports: [ MatSidenavModule, MatCheckboxModule, MatButtonModule, MatToolbarModule, MatFormFieldModule]
+  imports: [ MatSidenavModule, MatCheckboxModule, MatButtonModule, MatToolbarModule, MatFormFieldModule, MatInputModule],
+  exports: [ MatSidenavModule, MatCheckboxModule, MatButtonModule, MatToolbarModule, MatFormFieldModule, MatInputModule]
 })
 export class MaterialModule { }
--- src/app/moments/moments.component.ts
+++ src/app/moments/moments.component.ts
@@ -6,6 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./moments.component.scss']
 })
 export class MomentsComponent implements OnInit {
+  cards = [
+    {socTitle : 'Instagram', socText : 'share your moments'},
+    {socTitle : 'facebook', socText : 'publish yours'},
+    {socTitle : 'twiter', socText : 'tweet yours'}
+  ];
+
+  images = [
+    'https://cdn.pixabay.com/photo/2017/01/14/15/11/relaxing-1979674_960_720.jpg',
+    'https://cdn.pixabay.com/photo/2016/02/19/10/21/mug-1209194_960_720.jpg',
+    'https://cdn.pixabay.com/photo/2017/03/27/13/59/adult-2178904_960_720.jpg',
+    'https://cdn.pixabay.com/photo/2015/09/02/13/26/glasses-919071_960_720.jpg',
+    'https://cdn.pixabay.com/photo/2015/07/10/17/53/cheers-839865_960_720.jpg',
+    'https://cdn.pixabay.com/photo/2015/10/12/15/05/girls-984154_960_720.jpg',
+    'https://cdn.pixabay.com/photo/2015/10/12/14/57/campfire-984020_960_720.jpg',
+    'https://cdn.pixabay.com/photo/2014/05/03/00/42/van-336606_960_720.jpg',
+    'https://cdn.pixabay.com/photo/2016/11/22/19/15/audience-1850119_960_720.jpg'
+
+
+  ];
 
   constructor() { }
 
